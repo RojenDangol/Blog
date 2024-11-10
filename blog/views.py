1e79b351d28bcd53855e8c49ea4e2aa965994839
Rojen Dangol <rojendangol44@gmail.com>
2024-11-10 18:16:43 +0545

    new apps

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def blogHome(request):
+    return HttpResponse('We will keep blog here ')
+
+
+def blogPost(request, slug):
+    return HttpResponse('This is blogPost')
