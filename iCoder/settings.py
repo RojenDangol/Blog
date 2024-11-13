47ec90cbaa22c45ee99bc3830d12c16487ab5c39
Rojen Dangol <rojendangol44@gmail.com>
2024-11-13 12:51:23 +0545

    contact

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'home',
-    'blog',
+    'home.apps.HomeConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
