d69fc7903fa7a1704eea7f4c1fc268cd6bad39ca
Rojen Dangol <rojendangol44@gmail.com>
2024-11-14 19:17:21 +0545

    register user

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Contact
 from django.contrib import messages
+from django.contrib.auth.models import User
 from blog.models import Post
 
 # Create your views here.
@@ -41,3 +42,35 @@ def search(request):
         messages.warning(request, "No Search Results")
     params = {'allPosts':allPosts, 'query':query}
     return render(request, 'home/search.html', params)
+
+
+def handleSignup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        # check user validation
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters")
+            return redirect('/')
+        if not username.isalnum():
+            messages.error(request, "Username should not contain special characters")
+            return redirect('/')
+        if pass1 != pass2:
+            messages.error(request, "Password do not match")
+            return redirect('/')
+
+        # creating the user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, "Your account has been successfully created")
+        return redirect('/')
+    else:
+        return HttpResponse('404 - Page Not Found')
+    # return render(request, '')
